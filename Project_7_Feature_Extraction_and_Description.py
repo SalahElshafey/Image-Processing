4fa2e028b1551deb852ab3862bc7e208d6a72853
Salah Elshafey <112121144+SalahElshafey@users.noreply.github.com>
2024-01-04 14:25:22 +0200

    Create Project_7_Feature_Extraction_and_Description.py

---
+++ Project_7_Feature_Extraction_and_Description.py
@@ -0,0 +1,34 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img1 = cv2.imread('images/london bridge-1.jpg')
+img2 = cv2.imread('images/london bridge-2.jpg')
+
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+# SIFT
+sift = cv2.SIFT_create()
+
+keypoints_1, descriptors_1 = sift.detectAndCompute(img1, None)
+keypoints_2, descriptors_2 = sift.detectAndCompute(img2, None)
+
+
+#feature matching
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+matches = bf.match(descriptors_1,descriptors_2)
+matches = sorted(matches, key = lambda x:x.distance)
+
+img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2, matches[:50], img2, flags=2)
+plt.imshow(img3),plt.show()
+
+
+len(keypoints_1), len(keypoints_2)
+
+figure, ax = plt.subplots(1, 2, figsize=(16, 8))
+
+ax[0].imshow(cv2.drawKeypoints(img1, keypoints_1, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS), cmap='gray')
+ax[1].imshow(cv2.drawKeypoints(img2, keypoints_2, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS), cmap='gray')
+
+plt.show()
